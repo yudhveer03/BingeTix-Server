4be605f48ba351af62be3465cb96825033a7dabd
yudhveer03 <yudhveer0320@gmail.com>
2026-08-29 08:53:03 +0530

    Fixed minor bug

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ import movies from '../routes/movies.routes.js';
 import cors from 'cors';
 import bookingRoutes from '../routes/booking.routes.js';
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://binge-tix-client.vercel.app/"],
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/movie', movies)
 app.use('/api/booking', bookingRoutes);
-    
+
 export default app;
 
